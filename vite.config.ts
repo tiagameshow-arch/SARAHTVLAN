89baf774fc69458068854a7fe0806d0ae3b7b3f8
tiagameshow-arch <tiagameshow@gmail.com>
2026-05-25 16:46:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // O ponto garante que os caminhos sejam relativos
   build: {
     outDir: 'dist' // Garante que a pasta de saída é a correta
   }
