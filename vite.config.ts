bbdb7bcf8d7f74c0f932e1796a20cc6d65ec9465
tiagameshow-arch <tiagameshow@gmail.com>
2026-05-25 15:32:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SARAHTVLAN/', // ESSA LINHA É OBRIGATÓRIA
+  // Isto força o Vite a buscar os arquivos usando caminhos relativos (./)
+  // em vez de caminhos absolutos (/) que quebram no Vercel.
+  base: './', 
 })
