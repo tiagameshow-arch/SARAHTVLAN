604744eff3e839824d503f01f4631931e2518eed
tiagameshow-arch <tiagameshow@gmail.com>
2026-05-25 11:54:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // A linha abaixo é o segredo para o GitHub Pages funcionar:
-  base: '/SARAHTVLAN/', 
+  base: '/SARAHTVLAN/', // ESSA LINHA É OBRIGATÓRIA
 })
